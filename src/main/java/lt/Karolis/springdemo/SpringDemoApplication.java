920db24835087be604be448cfce25f4154856c1d
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-16 10:37:32 +0200

    Initial commit

---
+++ src/main/java/lt/Karolis/springdemo/SpringDemoApplication.java
@@ -0,0 +1,14 @@
+package lt.Karolis.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
